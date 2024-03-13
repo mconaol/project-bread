6fe0f0c3ba73e74cdb7beff6598bf36cd4c5d433
Mitzy Onaolapo <rios_mit@yahoo.com>
2024-03-12 20:34:30 -0700

    sending index and showing data

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+
+//Config 
+require('dotenv').config()
+const PORT = process.env.PORT
+const app = express()
+
+//Routes 
+app.get('/',(req,res)=>{
+    res.send('Welcome to an Awesome App about Breads!')
+})
+
+// ROUTES
+app.get('/', (req, res) => {
+    res.send('Welcome to an Awesome App about Breads')
+  })
+  
+  // Breads
+  const breadsController = require('./controllers/breads_controller.js')
+  app.use('/breads', breadsController)
+  
+
+//Listen 
+app.listen(PORT, ()=>{
+    console.log('listening on port',PORT);
+})
+
+console.log(PORT)
