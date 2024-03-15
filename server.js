d555c84fdffe0637c6697e7f9a9356f35763545e
Mitzy Onaolapo <rios_mit@yahoo.com>
2024-03-14 19:59:46 -0700

    create and new routes complete

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
+
 
 
 
@@ -17,16 +19,16 @@ app.use(express.static('public'))
 app.get('/', (req, res) => {
     res.send('Welcome to an Awesome App about Breads')
   })
-  
-  // 404 Page
-app.get('*', (req, res) => {
-  res.send('404')
-})
+
 
   // Breads
   const breadsController = require('./controllers/breads_controller.js')
   app.use('/breads', breadsController)
   
+  // 404 Page
+  app.get('*', (req, res) => {
+    res.send('404')
+  })
 
 //Listen 
 app.listen(PORT, ()=>{
--- views/layouts/Index.jsx
+++ views/layouts/Index.jsx
@@ -6,9 +6,18 @@ function Index ({breads})  {
       <Default>
         <h2>Index Page</h2>
         {/* <p>I have {breads[0].name} bread!</p> */}
+        <div className="newButton">
+  <a href="/breads/new"><button>Add a new bread</button></a>
+</div>
+
+<div className="backButton">
+  <a href="/breads"><button>Go back to the index</button></a>
+</div>
+
       </Default>
     )
 }
+
 
 
 module.exports = Index
